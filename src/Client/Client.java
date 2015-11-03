cdaa6d2309dcc1044e68bc1d88068fdca03d62f0
Lucas MARTINEZ <martinez.lucas@etu.unice.fr>
2015-11-03 14:07:46 +0100

    creation packages

---
+++ src/Client/Client.java
@@ -0,0 +1,8 @@
+package Client;
+
+/**
+ * @author Lucas MARTINEZ
+ * @version 03/11/15
+ */
+public class Client {
+}
---
+++ src/Server/Server.java
@@ -0,0 +1,8 @@
+package Server;
+
+/**
+ * @author Lucas MARTINEZ
+ * @version 03/11/15
+ */
+public class Server {
+}
